1ffe78a8cc9a9f950798cb2064b68fe402a59e8c
ilonaand <ilonaand@gmail.com>
2023-01-21 22:25:45 +0300

    feat:add webscket

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,8 +1,10 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
+import { WebSocketServer } from 'ws';
+import { mouse } from "@nut-tree/nut-js";
 
-export const httpServer = http.createServer(function (req, res) {
+export const httpServer = http.createServer(function (req: http.IncomingMessage, res: http.ServerResponse) {
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
     fs.readFile(file_path, function (err, data) {
@@ -13,5 +15,12 @@ export const httpServer = http.createServer(function (req, res) {
         }
         res.writeHead(200);
         res.end(data);
+    });
+});
+
+const wss = new WebSocketServer({ server: httpServer });
+wss.on('connection', (ws) => {
+    ws.on('message', (data) => {
+      console.log('received: %s', data);
     });
 });
